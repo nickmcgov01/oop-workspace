1a7fe3bb42c74f60e769d9c45f07d35f8243be54
nickmcgov01 <nicholas@Nicholas>
2024-08-28 17:15:32 +0930

    workshop6 commit final

--- Player.h
+++ Player.h
@@ -15,28 +15,40 @@ class Player{
     }
 
     void attack(Player* opponent, int damage){
-        opponent -= damage;
+        std::cout << name << " attacks " << opponent->getName() << " for " << damage << " damage!" << std::endl;
+        opponent->takeDamage(damage);
+        
     }
-
+    
     void takeDamage(int damage){
         health -= damage;
+        std::cout << name << " took " << damage << " damage, remaining health is " << health << std::endl;
     }
 
-    int getHealth(int addedHealth){
-        health += addedHealth;
+    std::string getName(){
+        return name;
+    }
+
+    int getHealth(){
         return health;
     }
 
+    int getDamage(){
+        return damage;
+    }
 
-    
 };
 
 class Wizard : public Player{
     private:
     int mana;
     public:
+   Wizard(std::string playerName, int playerHealth, int playerDamage, int playerMana) 
+    : Player(playerName, playerHealth, playerDamage), mana(playerMana) {}
+
     void castSpell(Player* opponent){
-        opponent -= mana;
+        std::cout << name << " used spell! " << " Opponent took "  << mana << " Damage " << std::endl;
+        opponent->takeDamage(mana);
     }
 };
 
@@ -44,7 +56,13 @@ class Warrior : public Player{
     private:
     std::string weapon;
     public:
+     Warrior(std::string playerName, int playerHealth, int playerDamage, std::string playerWeapon) 
+        : Player(playerName, playerHealth, playerDamage), weapon(playerWeapon) {}
     void swingWeapon(Player* opponent){
-        opponent -= damage;
+        std::cout << name << " swings their " << weapon << " Opponent took " << damage << " damage " << std::endl;
+        opponent ->takeDamage(damage);
+    }
+    std::string getWeapon() const {
+        return weapon;
     }
 };
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,7 +1,35 @@
-#include "Player.h"
+#include "Player.h"  
 #include <iostream>
-int main(void){
-    Player warrior("Joe", 100, 10);
-    Player wizard("Harry", 100,10);
-    
+
+int main() {
+    // Step 1: Create instances of Wizard and Warrior
+    Wizard wizard("Gandalf", 100, 10, 20);    // Name: Gandalf, Health: 100, Damage: 10, Mana: 20
+    Warrior warrior("Aragorn", 120, 15, "Sword");  // Name: Aragorn, Health: 120, Damage: 15, Weapon: Sword
+
+    // Step 2: Simulate the battle
+    while (wizard.getHealth() > 0 && warrior.getHealth() > 0) {
+   
+        wizard.castSpell(&warrior);  
+       
+        if (warrior.getHealth() <= 0) {
+            std::cout << warrior.getName() << " has been defeated!" << std::endl;
+            break;  
+
+        warrior.swingWeapon(&wizard);  
+
+      
+        if (wizard.getHealth() <= 0) {
+            std::cout << wizard.getName() << " has been defeated!" << std::endl;
+            break;  
+        }
+    }
+
+  
+    if (wizard.getHealth() > 0) {
+        std::cout << wizard.getName() << " is the winner!" << std::endl;
+    } else {
+        std::cout << warrior.getName() << " is the winner!" << std::endl;
+    }
+
+    return 0;
 }
