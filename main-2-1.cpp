b5abd6eae45e6301f676e9e4174a329daea24918
nickmcgov01 <a1942026@adelaide.edu.au>
2024-09-04 00:11:57 +0930

    next commit

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -3,11 +3,11 @@
 
 int main(void){
     Fridge joe;
-    std::cout << " Volume before: " << joe.getVolume() << std::endl;
+    std::cout << "Volume before: " << joe.getVolume() << std::endl;
     joe.setVolume(100);
-    std::cout << " Volume after: " << joe.getVolume() << std::endl;
-    joe.set_PowerRating(1000);
-    std::cout << "Power rating: " << joe.get_PowerRating() << "Power consumption: " << joe.getPowerConsumption() << std::endl;
+    std::cout << "Volume after: " << joe.getVolume() << std::endl;
+    joe.set_powerRating(1000);
+    std::cout << "Power rating: " << joe.get_powerRating() << "\nPower consumption: " << joe.getPowerConsumption() << std::endl;
 
 
 
