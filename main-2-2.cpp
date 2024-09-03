8a2e360ef88b831d225941d43fd2299aae904494
nickmcgov01 <a1942026@adelaide.edu.au>
2024-09-04 00:24:22 +0930

    another commit lol

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -6,8 +6,8 @@ int main(void){
     std::cout << "Screen size before: " << samsung.getScreenSize() << std::endl;
     samsung.setScreenSize(100);
     std::cout << "Screen size after: " << samsung.getScreenSize() << std::endl;
-    samsung.set_PowerRating(1000);
-    std::cout << "Power rating: " << samsung.get_PowerRating() << "\nPower consumption: " << samsung.getPowerConsumption() << std::endl;
+    samsung.set_powerRating(1000);
+    std::cout << "Power rating: " << samsung.get_powerRating() << "\nPower consumption: " << samsung.getPowerConsumption() << std::endl;
 
     return 0;
 }
