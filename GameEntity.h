58588c3f28d9c56f1fb709402da3ae6ecce6f1f2
nickmcgov01 <nicholas@Nicholas>
2024-10-22 23:31:50 +1030

    second commit

--- GameEntity.h
+++ GameEntity.h
@@ -1,31 +1,34 @@
-#include <iostream>
 #include <tuple>
+
 #ifndef GAMEENTITY_H
 #define GAMEENTITY_H
 
 class GameEntity {
-    public:
-enum GameEntityType {
-    ExplosionType,
-    MineType,
-    NoneType,
-    ShipType,
-};
-private:
-    std::tuple<int,int> position;
-    GameEntityType type;
 public:
-    GameEntity(int x, int y, GameEntityType type) {
-        position = std::make_tuple(x,y);
-        switch(type) {
-            case 
-        }
-    }
-    std::tuple<int, int> getPos(){
+    // Public enum so that it can be accessed externally
+    enum GameEntityType {
+        ExplosionType,
+        MineType,
+        NoneType,
+        ShipType
+    };
+
+private:
+    std::tuple<int, int> position;
+    GameEntityType entityType;  // Renamed member variable to avoid shadowing
+
+public:
+    // Constructor using initializer list
+    GameEntity(int x, int y, GameEntityType type)
+        : position(std::make_tuple(x, y)), entityType(type) {}
+
+    std::tuple<int, int> getPos() {
         return position;
     }
-    GameEntityType getType(){
-        return type;
+
+    GameEntityType getType() {
+        return entityType;
     }
 };
+
 #endif
--- main-1.cpp
+++ main-1.cpp
@@ -1,31 +1,17 @@
 #include "GameEntity.h"
-#include "Utils.h"
 #include <iostream>
-#include <tuple>
-#include <cstdlib>
-#include <ctime>
 
 int main() {
-    srand(static_cast<unsigned int>(time(0)));  // Seed for randomness
+    // Creating GameEntity objects with different types
+    GameEntity entity1(10, 20, GameEntity::ShipType);
+    GameEntity entity2(30, 40, GameEntity::MineType);
 
-    // Generating random positions
-    auto pos1 = Utils::generateRandomPos(100, 100);
-    auto pos2 = Utils::generateRandomPos(100, 100);
-
-    // Creating two GameEntity objects
-    GameEntity entity1(std::get<0>(pos1), std::get<1>(pos1), GameEntity::ShipType);
-    GameEntity entity2(std::get<0>(pos2), std::get<1>(pos2), GameEntity::MineType);
-
-    // Output the positions and types
+    // Output entity positions and types
     std::cout << "Entity 1 Position: (" << std::get<0>(entity1.getPos()) << ", " 
               << std::get<1>(entity1.getPos()) << "), Type: Ship\n";
 
     std::cout << "Entity 2 Position: (" << std::get<0>(entity2.getPos()) << ", " 
               << std::get<1>(entity2.getPos()) << "), Type: Mine\n";
-
-    // Calculating and outputting the distance between the two entities
-    double distance = Utils::calculateDistance(entity1.getPos(), entity2.getPos());
-    std::cout << "Distance between Entity 1 and Entity 2: " << distance << std::endl;
 
     return 0;
 }
